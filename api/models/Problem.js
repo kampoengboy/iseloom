6cf57fb5de063fc6a87771374017226993e55ed2
Michael <mike_visualsoft@yahoo.com>
2016-02-05 16:52:56 +0700

    add status on problem.js

--- api/models/Problem.js
+++ api/models/Problem.js
@@ -13,7 +13,11 @@ module.exports = {
       timelimit : 'integer',
       description : 'string',
       input : 'array',
-      output : 'array'
+      output : 'array',
+      status : {
+          type : 'boolean',
+          defaultsTo : false
+      }
   }
 };
 
